7fa11ef451c942af5d5b65ca509c922d51d9d21f
yellowb <122517167@qq.com>
2018-06-08 16:54:31 +0800

    Add background.js

---
+++ js/background.js
@@ -0,0 +1,8 @@
+let extensionName = 'ZhuHai Bus Notifier';
+
+// Init logic here when extension installed.
+chrome.runtime.onInstalled.addListener(() => {
+    console.log(`${extensionName} is installed.`);
+    // TODO
+    // Might popup notification to remind user to add bus lines
+});
