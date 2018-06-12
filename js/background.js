b9bf579f1da0a461d1f406dcfa291c442da54808
yellowb <122517167@qq.com>
2018-06-12 15:10:07 +0800

    Implementing Options UI

--- js/background.js
+++ js/background.js
@@ -1,8 +1,17 @@
 const extensionName = 'ZhuHai Bus Notifier';
 
+
+/**
+ * Init some data in local storage.
+ */
+function initStorage() {
+    chrome.storage.local.set({'watchedLines': []});
+    console.log(`Local Storage has been initialized.`);
+}
+
 // Init logic here when extension installed.
 chrome.runtime.onInstalled.addListener(() => {
     console.log(`${extensionName} is installed.`);
     // TODO
-    // Might popup notification to remind user to add bus lines
+    initStorage();
 });
--- js/options.js
+++ js/options.js
@@ -1,3 +1,63 @@
+const KEY_FOR_WATCHED_LINES = 'watchedLines';
+const BUS_CURRENT_STATUS_URL = 'http://test.zhbuswx.com/RealTime/GetRealTime'
+
+let busLineNameField = $('#line-setting-panel-line-input');
+let busLineFromStationField = $('#line-setting-panel-from-station-input');
+
+/**
+ * Save user input into watched lines
+ */
+function saveBusLine() {
+    let busLineName = busLineNameField.val().toUpperCase();
+    let busLineFromStation = busLineFromStationField.val();
+    let key = busLineName + '__' + busLineFromStation;
+
+    async.waterfall([
+        function (callback) {
+            chrome.storage.local.get(KEY_FOR_WATCHED_LINES, function (object) {
+                if (_.isEmpty(object)) {
+                    return callback(null, []);
+                }
+                else {
+                    return callback(null, object[KEY_FOR_WATCHED_LINES]);
+                }
+            });
+        },
+        // Save into local storage if not existed.
+        function (existingWatchedLines, callback) {
+            console.log('Get existing watchedLines from local storage: ' + JSON.stringify(existingWatchedLines));
+            if (!_.some(existingWatchedLines, function (item) {
+                return _.isEqual(key, item.key);
+            })) {
+                existingWatchedLines.push(constructWatchedLine(busLineName, busLineFromStation));
+                let cache = {};
+                cache[KEY_FOR_WATCHED_LINES] = existingWatchedLines;
+                chrome.storage.local.set(cache, function () {
+                    return callback(null);
+                });
+            }
+            else {
+                console.log(`Bus line ${busLineName} + ${busLineFromStation} is already in watched lines!`);
+                return callback(null);
+            }
+        }
+    ], function (err, result) {
+        // result now equals 'done'
+    });
+}
+
+function constructWatchedLine(lineNumber, fromStation) {
+    return {
+        'lineNumber': lineNumber,
+        'fromStation': fromStation,
+        'key': lineNumber + '__' + fromStation
+    }
+}
+
+
+// For message can close
+$('#line-setting-panel-save-btn').on('click', saveBusLine);
+
 // For message can close
 $('.close.icon').on('click', function () {
     $(this).parent().hide();
